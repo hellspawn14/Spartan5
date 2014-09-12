20140496eb63fcefc0578bbe7814c3e213ff6ee0
hellspawn14 <hellspawn@192.168.0.26>
2014-09-12 16:09:46 -0500

    nueva vista

---
+++ Spartan5/src/com/spartan/android/CheckInActivity.java
@@ -0,0 +1,35 @@
+package com.spartan.android;
+
+import com.example.spartan5.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CheckInActivity extends Activity
+{
+	//-----------------------------------------------------------------
+	//Constantes
+	//-----------------------------------------------------------------
+
+	//-----------------------------------------------------------------
+	//Atributos
+	//-----------------------------------------------------------------
+
+	//-----------------------------------------------------------------
+	//Constructor
+	//-----------------------------------------------------------------
+
+	/**
+	 * Metodo para la creacion de la actividad
+	 */
+	protected void onCreate(Bundle savedInstanceState) 
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_check_in);
+	}
+	
+	//-----------------------------------------------------------------
+	//Metodos
+	//-----------------------------------------------------------------
+
+}

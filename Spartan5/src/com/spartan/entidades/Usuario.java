d32106e2951290aca7e7ff4cb4abd9a4be2f5c1d
hellspawn14 <hellspawn@192.168.0.26>
2014-09-12 10:19:49 -0500

    Creacion de entidades

---
+++ Spartan5/src/com/spartan/entidades/Usuario.java
@@ -0,0 +1,40 @@
+package com.spartan.entidades;
+
+import java.util.ArrayList;
+
+/**
+ * Representa al usuario
+ * @author hellspawn
+ */
+public class Usuario 
+{
+	
+	//-----------------------------------------------------------------
+	//Constantes
+	//-----------------------------------------------------------------
+
+	//-----------------------------------------------------------------
+	//Atributos
+	//-----------------------------------------------------------------
+
+	/**
+	 * Lista de eventos por asistir
+	 */
+	private ArrayList <Asistencia> asistencias;
+	
+	/**
+	 * Lista de eventos creados por el usuario
+	 */
+	private ArrayList <EventoPrivado> eventosUsuario;
+	 
+	//-----------------------------------------------------------------
+	//Constructores
+	//-----------------------------------------------------------------
+	
+
+	//-----------------------------------------------------------------
+	//Metodos
+	//-----------------------------------------------------------------
+	
+
+}

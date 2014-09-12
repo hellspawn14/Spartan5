18efc271296fd45a38cd183eaabb8530d1bddbb4
hellspawn14 <hellspawn@192.168.0.26>
2014-09-11 23:52:00 -0500

    Cambios

---
+++ Spartan5/src/com/spartan/recursos/Eventos.java
@@ -0,0 +1,33 @@
+package com.spartan.recursos;
+
+/**
+ * Enumeración con los eventos disponibles en la aplicacion
+ * @author hellspawn
+ */
+public class Eventos 
+{
+	//-----------------------------------------------------------------
+	//Constantes
+	//-----------------------------------------------------------------
+
+	/**
+	 * Futbol
+	 */
+	public final static String SOCCER = "Fútbol";
+	
+	/**
+	 * Basketball
+	 */
+	public final static String BASKET = "Baloncesto";
+	
+	/**
+	 * Voleyball
+	 */
+	public final static String VOLEY = "Voleibol";
+	
+	/**
+	 * Tennis
+	 */
+	public final static String TENNIS = "Tenis";
+
+}

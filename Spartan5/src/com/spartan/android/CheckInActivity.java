c292ab70b1aa7580dbda8ce07d9dd1119e0b5104
hellspawn14 <hellspawn@192.168.0.26>
2014-09-14 00:44:17 -0500

    Faltan varias cosas - Mostrar detalle de asistencias - Eliminar asistencias - (Cancelar) - Hacer checkin asistencias - Persistir asistencias - Enviar mensajes - Actualizar juegos y score - Persistir score

--- Spartan5/src/com/spartan/android/CheckInActivity.java
+++ Spartan5/src/com/spartan/android/CheckInActivity.java
@@ -1,9 +1,11 @@
 package com.spartan.android;
 
 import com.example.spartan5.R;
+import com.spartan.entidades.Spartan;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class CheckInActivity extends Activity
 {
@@ -15,6 +17,20 @@ public class CheckInActivity extends Activity
 	//Atributos
 	//-----------------------------------------------------------------
 
+	/**
+	 * ListView para mostrar los resultados
+	 */
+	private ListView asistencias;
+	
+	/**
+	 * Instancia de la clase principal 
+	 */
+	private Spartan instanciaSpartan;
+	
+	/**
+	 * Colección de elementos para poner en la lista
+	 */
+	private String [] items;
 	//-----------------------------------------------------------------
 	//Constructor
 	//-----------------------------------------------------------------
